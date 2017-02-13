09eb4103618f655d12e1d33bc58c684dc581fb84
SlippyJohny <shpak.d.o@ukr.net>
2017-02-13 15:19:58 +0200

    assets sett

--- config/application.rb
+++ config/application.rb
@@ -10,5 +10,6 @@ module Systems
   class Application < Rails::Application
     config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', {expires_in: 90.minutes}
     config.autoload_paths << Rails.root.join('lib')
+    config.serve_static_assets = true
   end
 end
