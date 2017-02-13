7c8503a88b8c11dde152481e5d5bf24f776a21c2
SlippyJohny <shpak.d.o@ukr.net>
2017-02-13 15:58:47 +0200

    asset

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module Systems
     config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', {expires_in: 90.minutes}
     config.autoload_paths << Rails.root.join('lib')
     config.serve_static_assets = true
+
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,12 +24,9 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'mailersystem.herokuapp.com' }
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-
-  config.serve_static_assets=true
   config.assets.compress=true
-  config.assets.compile =true
-  config.assets.initialize_on_precompile=false
-  config.assets.digest=true
+  config.assets.compile = false
+  config.assets.digest = true
   config.action_dispatch.x_sendfile_header='X-Accel-Redirect'
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
