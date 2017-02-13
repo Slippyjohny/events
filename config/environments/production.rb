d08b3ee071953406b66c231442870ab2503667fb
SlippyJohny <shpak.d.o@ukr.net>
2017-02-13 15:08:44 +0200

    assets sett

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
 
-  config.public_file_server.enabled = true
+  config.public_file_server.enabled = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
