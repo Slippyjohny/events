d534cbe4110f9577d4b72e70093694d72156eff5
SlippyJohny <shpak.d.o@ukr.net>
2017-02-20 20:15:13 +0200

    added Jquery while event  creating

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -5,15 +5,13 @@ describe Event do
   it "time shoud be valid" do
     time=Time.now+2.hour
     end_time=time+1.day
-    user=create(:user)
-    event=create(:event, time: "#{time}", end_time: "#{end_time}")
+    create(:event, time: "#{time}", end_time: "#{end_time}")
   end
 
-  it "time shoud_not  be valid" do
+  it "time shoud_not be valid" do
     time=Time.now
     end_time=time+1.minute
-    user=create(:user)
-    event=create(:event, time: "#{time}", end_time: "#{end_time}")
+    create(:event, time: "#{time}", end_time: "#{end_time}")
   end
 
 end
